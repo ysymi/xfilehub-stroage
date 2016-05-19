1ff9cefd40fa513b62d9dc64b1ae1b7fb7d9eae9
ysymi <ff.2011@live.cn>
2016-05-19 21:29:24 +0800

    write template in jinja2

--- server.py
+++ server.py
@@ -1,44 +1,23 @@
-import logging
-
-# import tornado.web
-# import tornado.ioloop
-#
-# from handler.main import MainHandler
-# from handler.upload import UploadHandler
-# from handler.download import DownloadHandler
-# from config import PORT
-#
-#
-# def make_app():
-#     settings = {
-#         'gzip': True,
-#         'debug': True,
-#         'static_path': 'static/',
-#         'template_path': 'template/'
-#     }
-#     return tornado.web.Application([
-#         (r'/', MainHandler),
-#         (r'/upload', UploadHandler),
-#         (r'/download', DownloadHandler),
-#     ], **settings)
-#
-#
-# if __name__ == '__main__':
-#     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
-#     app = make_app()
-#     app.listen(PORT)
-#     tornado.ioloop.IOLoop.current().start()
-#
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    file_list = []  # block_index.get().keys()
+    return render_template('index.html', file_list=file_list)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    pass
+
+
+@app.route('/download/<filename>')
+def download(filename):
+    pass
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=9000, debug=True)
