cd3a734d2605813b1c53c2ac10f49022d493343e
ysymi <ff.2011@live.cn>
2016-05-19 21:26:14 +0800

    move handler into server

--- handler/main.py
+++
@@ -1,9 +0,0 @@
-from tornado.web import RequestHandler
-
-from block.block import block_index
-
-
-class MainHandler(RequestHandler):
-    def get(self):
-        file_list = block_index.get().keys()
-        self.render('index.html', file_list=file_list)
