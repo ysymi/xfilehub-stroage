c3e2413d9073fcee1294676fe33bb6436e0b9d3b
ysymi <ff.2011@live.cn>
2016-05-23 19:57:13 +0800

    add lots of config

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
-HOST = 'localhost'
-PORT = 5000
+import os
+
 DEBUG = True
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+HOST = 'localhost'
+PORT = int(os.environ['PORT'])
+ALL_PORTS = list(range(8000, 8010))
+
+ROOT = os.path.dirname(__file__)
+ROOT = os.path.join(ROOT, 'storages', 'storage#%s' % PORT)  # collect all storage dir
+STORAGE_DIR = os.path.join(ROOT, 'chunks')
+CHUNK_NOTE_PATH = os.path.join(ROOT, 'chunk.note')
+
+BUFFER_SIZE = 1024 * 1024
+CHUNK_NAME_FORMAT = '%s.#$s'
+
+LOGGING_FORMAT = '%(asctime)s  %(levelname)s  %(message)s'
+APP_INFO = {'host': HOST, 'port': PORT}
