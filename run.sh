2cd9676fc28cb3b9011ea108e8742d4d6570a69c
ysymi <ff.2011@live.cn>
2016-05-24 18:18:45 +0800

    remove sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo
-for port in $(seq 8000 8009)
-do
-    PORT=${port} python3.5 server.py &
-done
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for port in $(seq 8000 8009)
-do
-    kill -9 $(lsof -i:${port} | awk '{print $2}' | tail -n 2)
-done
-lsof -nPi TCP -s TCP:LISTEN | grep 127.0.0.1:80 | grep 4u | sort -k 8
