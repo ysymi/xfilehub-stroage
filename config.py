815af49e19c21486cf5f9b133ab0f0ac1437d733
ysymi <ff.2011@live.cn>
2016-05-24 18:18:18 +0800

    set new chunkname

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ALL_PORTS = list(range(8000, 8010))
 ROOT = os.path.dirname(__file__)
 ROOT = os.path.join(ROOT, 'storages', 'storage#%s' % PORT)  # collect all storage dir
 STORAGE_DIR = os.path.join(ROOT, 'chunks')
-CHUNK_NOTE_PATH = os.path.join(ROOT, 'chunk.note')
+CHUNK_NOTE_PATH = os.path.join(ROOT, 'chunks.note')
 
 CHUNK_NAME_FORMAT = '{filename}.chunk{seq:0>3}'
 LOGGING_FORMAT = '%(asctime)s  %(levelname)s  %(message)s'
