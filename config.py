7428704c9620c99640cd42c6671e96446e204740
ysymi <ff.2011@live.cn>
2016-05-31 18:29:29 +0800

    fix bug: add localhost for host

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
-import socket
 
 DEBUG = True
-
-HOST = socket.gethostbyname(socket.gethostname())
+GET_IP_CMD = 'ifconfig| grep 192.168 | awk \'{print $2}\''
+HOST = os.popen(GET_IP_CMD).read().strip() or 'localhost'
 PORT = int(os.environ['PORT'])
 ALL_PORTS = list(range(8000, 8010))
 
@@ -13,7 +12,7 @@ STORAGE_DIR = os.path.join(ROOT, 'chunks')
 CHUNK_NOTE_PATH = os.path.join(ROOT, 'chunks.note')
 
 CHUNK_NAME_FORMAT = '{filename}.chunk{seq:0>3}'
-LOGGING_FORMAT = '%(asctime)s  %(levelname)s  %(message)s'
+LOGGING_FORMAT = '%(asctime)s %(levelname)s %(message)s'
 APP_INFO = {'host': HOST, 'port': PORT}
 
 BUFFER_SIZE = 1024 * 1024
